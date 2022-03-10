bbe0e21fa598c0924ad204498b4b475717844783
Jonah Foster <jonahfoster97@gmail.com>
2022-03-10 11:43:42 -0600

    Added test for manager

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,7 @@
+const Manager = require('../lib/Manager');
+
+describe('Manager class', () => {
+    it('getOfficeNumber should return manager office number', () => {
+        expect(new Manager('Kanye', 3, 'kanye@gmail.com', 312).getOfficeNumber()).toBe(312);
+    });
+})
